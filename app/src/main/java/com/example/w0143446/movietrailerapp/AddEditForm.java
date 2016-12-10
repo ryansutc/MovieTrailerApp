c884b93e99c07e41c37fd0275aa360509f539aa2
w0143446 <w0143446@nscc.ca>
2016-12-09 21:30:27 -0400

    Updates to video player, added edit form and changed dummy data structure. BETA 0.1

--- app/src/main/java/com/example/w0143446/movietrailerapp/AddEditForm.java
+++ app/src/main/java/com/example/w0143446/movietrailerapp/AddEditForm.java
@@ -1,14 +1,33 @@
 package com.example.w0143446.movietrailerapp;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class AddEditForm extends AppCompatActivity {
+    private Integer videoID;
+    private String videoName;
+    private String videoDesc;
+    private float videoRating;
+    private String videoUrl;
+    private String videoCat;
+    private String videoThumb;
 
+    EditText etName;
+    EditText etDesc;
+    RatingBar videoRatingBar;
+    EditText etURL;
+    EditText etCategory;
+    EditText etThumb;
+
+    DBAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +35,27 @@ public class AddEditForm extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        etName = (EditText) findViewById(R.id.etName);
+        etDesc = (EditText) findViewById(R.id.etDesc);
+        videoRatingBar = (RatingBar) findViewById(R.id.videoRatingBar);
+        etURL = (EditText) findViewById(R.id.etURL);
+        etCategory = (EditText) findViewById(R.id.etCategory);
+        etThumb = (EditText) findViewById(R.id.etThumb);
+
+        //handle intent
+        Intent myintent = getIntent();
+        videoID = myintent.getIntExtra("videoID", 0);
+        adapter = new DBAdapter(this);
+        adapter.open();
+            Cursor cursor = adapter.getVideo(videoID);
+            etName.setText(cursor.getString(1));
+            etDesc.setText(cursor.getString(2));
+            videoRatingBar.setStepSize(0.5f);
+            videoRatingBar.setRating(cursor.getFloat(3));
+            etURL.setText(cursor.getString(4));
+            etCategory.setText(cursor.getString(5));
+            etThumb.setText(cursor.getString(6));
+        adapter.close();
     }
 
 }
--- app/src/main/java/com/example/w0143446/movietrailerapp/DummyData.java
+++ app/src/main/java/com/example/w0143446/movietrailerapp/DummyData.java
@@ -13,23 +13,23 @@ public class DummyData {
         //http://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
         //(String name, String desc, int rating, String link, String category, int thumbnail)
         long id = db.insertVideo("Star Wars", "Some stupid movie about people a big hairy chewbaca",
-                1.5f, "https://www.youtube.com/watch?v=frdj1zb9sMY",
+                1.5f, "frdj1zb9sMY",
                 "Adventure","https://img.youtube.com/vi/frdj1zb9sMY/sddefault.jpg");
 
         id = db.insertVideo("Peep Show - Ergonomic Keyboard", "Jez and Mark try to impress a girl in the elevator",
-                4f, "https://www.youtube.com/watch?v=pkK23-g-r7A",
+                4f, "pkK23-g-r7A",
                 "Adventure","https://img.youtube.com/vi/pkK23-g-r7A/sddefault.jpg");
 
         id = db.insertVideo("It's Always Sunny - The D.E.N.N.I.S. System", "Dennis explains his system of attracting girls",
-                3f, "https://www.youtube.com/watch?v=95Fx2aYQbQs",
+                3f, "95Fx2aYQbQs",
                 "Adventure","https://img.youtube.com/vi/95Fx2aYQbQs/sddefault.jpg");
 
         id = db.insertVideo("Officespace - The Photocopier Beatdown (CLEAN)", "The nerds enact revenge on photocopier mafia style",
-                3f, "https://www.youtube.com/watch?v=fjsSr3z5nVk",
+                3f, "fjsSr3z5nVk",
                 "Adventure","https://img.youtube.com/vi/v=fjsSr3z5nVk/sddefault.jpg");
 
         id = db.insertVideo("Kids in the Hall - Bad Doctor", "The I'm a bad doctor skit with Dave Foley",
-                5f, "https://www.youtube.com/watch?v=Pbjypn9JtKE",
+                5f, "Pbjypn9JtKE",
                 "Adventure","https://img.youtube.com/vi/v=Pbjypn9JtKE/sddefault.jpg");
         db.close();
     }
